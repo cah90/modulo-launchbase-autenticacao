f140e8e00c1d1921f69af85b356e14085355315f
Cássia <cascrisbern@gmail.com>
2020-11-24 21:58:45 -0300

    alert message and hash of password

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,8 +5,14 @@ module.exports = {
     return res.render("user/register")
   },
 
+  show(req,res) {
+    return res.send('ok, cadastrado')
+  },
+
   async post(req,res) {
 
-    return res.send('passed')
+    const userId = await User.create(req.body)
+
+    return res.redirect('/users')
   }
 }
--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,34 +1,57 @@
 const User = require('../models/user')
 
-async function post(req,res,next) {
+async function post(req, res, next) {
+
   //check if it has all the fields filled
   const keys = Object.keys(req.body)
   console.log('req.body', req.body)
 
-  for(key of keys) {
-    if(req.body[key] == "") {
-      return res.send('Please, fill all the fields.')
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.render('user/register', {
+        user: req.body,
+        error: "Preencha todos os campos."
+      })
+
     }
   }
 
   //check if the user exists 
-  let { email, cpf_cnpj, password, passwordRepeat } = req.body
-  
+  let {
+    email,
+    cpf_cnpj,
+    password,
+    passwordRepeat
+  } = req.body
+
   cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
-  
+
   const user = await User.findOne({
-    where: {email},
-    or: {cpf_cnpj}
+    where: {
+      email
+    },
+    or: {
+      cpf_cnpj
+    }
   })
 
-  if(user) return res.send('User already exists')
+  if (user) return res.render('user/register', {
+    user: req.body,
+    error: "Usuário já cadastrado."
+  })
 
   //check if password match
-  if(password != passwordRepeat) {
-    return res.send("Passwords are not equal")
-  } 
+  if (password != passwordRepeat) {
+    return res.render('user/register', {
+      user: req.body,
+      error: "Senha e confirmação não são as mesmas."
+    })
 
-  next()
+
+    next()
+  }
 }
 
-module.exports = { post }
+module.exports = {
+  post
+}
