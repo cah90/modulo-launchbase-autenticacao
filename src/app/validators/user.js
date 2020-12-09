130521ecff52bd4dba363566a71cc88d5309d73e
Cássia <cascrisbern@gmail.com>
2020-12-09 13:39:48 -0300

    user validations

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,18 +1,25 @@
 const User = require('../models/user')
+const { compare } = require('bcryptjs')
+
+function checkAllFields(body) {
+  const keys = Object.keys(body)
+
+  for (key of keys) {
+    if (body[key] == "") {
+      return {
+        user: body,
+        error: "Preencha todos os campos."
+      }
+    }
+  }
+}
 
 async function post(req, res, next) {
 
   //check if it has all the fields filled
-  const keys = Object.keys(req.body)
-
-  for (key of keys) {
-    if (req.body[key] == "") {
-      return res.render('user/register', {
-        user: req.body,
-        error: "Preencha todos os campos."
-      })
-
-    }
+  const fillAllFields = checkAllFields(req.body)
+  if(fillAllFields) {
+    return res.render("user/register", fillAllFields)
   }
 
   //check if the user exists 
@@ -63,7 +70,41 @@ async function show(req, res, next) {
   next()
 }
 
+async function update(req, res, next) {
+
+  //check if all fields are filled
+  const fillAllFields = checkAllFields(req.body)
+    if(fillAllFields) {
+      return res.render("user/index", fillAllFields)
+    }
+
+  // check the passoword filled 
+  const { id, password } = req.body
+
+  if(!password) {
+    return res.render("user/index", {
+      user: req.body,
+      error: "Coloque sua senha para atualizar seu cadastro."
+    } )
+  }
+
+  // check if the passwords match
+  const user = await User.findOne({where: {id}})
+
+  const passed = await compare(password, user.password)
+
+  if(!passed) return res.render("user/index", {
+    user: req.body,
+    error: "Senha incorreta."
+  })
+
+  req.user = user
+
+  next()
+}
+
 module.exports = {
   post,
-  show
+  show,
+  update
 }
