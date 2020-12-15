8ba6b7b0beb3664df8636a4f46cf84bd2133eac0
Cássia <cascrisbern@gmail.com>
2020-12-15 15:18:10 -0300

    login, logout, block and redirect of users

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,12 +5,13 @@ const multer = require('../app/middlewares/multer')
 const ProductController = require('../app/controllers/ProductController')
 const SearchController = require('../app/controllers/SearchController')
  
+const { onlyUsers } = require('../app/middlewares/session')
 
 // SEARCH
 routes.get('/search', SearchController.index)
 
 // PRODUCTS
-routes.get('/create', ProductController.create)
+routes.get('/create', onlyUsers, ProductController.create)
 routes.post('/', multer.array('photos', 6), ProductController.post)
 
 routes.get('/:id', ProductController.show)
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,12 +4,15 @@ const routes = express.Router()
 const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
  
-const Validator = require('../app/validators/user')
+const UserValidator = require('../app/validators/user')
+const SessionValidator = require('../app/validators/session')
+
+const { isLoggedRedirectToUsers } = require('../app/middlewares/session')
  
 // Login / Logout
-// routes.get('/login', SessionController.loginForm)
-// routes.post('/login', SessionController.login)
-// routes.post('/logout', SessionController.logout)
+routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
+routes.post('/login', SessionValidator.login, SessionController.login)
+routes.post('/logout', SessionController.logout)
  
 // Reset password / forgot
 // routes.get('/forgot-password', SessionController.forgotForm)
@@ -19,10 +22,10 @@ const Validator = require('../app/validators/user')
  
 // User register
 routes.get('/register', UserController.registerForm)
-routes.post('/register', Validator.post, UserController.post)
+routes.post('/register', UserValidator.post, UserController.post)
  
-routes.get('/', Validator.show, UserController.show)
-routes.put('/', Validator.update, UserController.update)
+routes.get('/', UserValidator.show, UserController.show)
+routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
  
 module.exports = routes
