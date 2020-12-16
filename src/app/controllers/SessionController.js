85216878d7c59f1aa0ecd0876e19f3bd04b0b04a
Cássia <cascrisbern@gmail.com>
2020-12-16 15:16:10 -0300

    using nodemailer to send emails

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto")
+const mailer = require("../lib/mailer")
 
 module.exports = {
   loginForm(req,res) {
@@ -37,7 +38,23 @@ module.exports = {
     })
 
     //enviar um email com um link de recuperação de senha
+    await mailer.sendMail({
+      to: user.email,
+      from: 'no-reply@launchstore.com.br',
+      subject: 'Recuperação de senha.',
+      html: `<h2>Perdeu a chave?</h2>
+      <p>Não se preocupe, clique no link abaixo para recuperar sua senha.</p>
+      <p>
+        <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
+          RECUPERAR SENHA
+        </a>
+      </p> 
+      `
+    })
 
     //avisar o usuário que enviamos o email
+    return res.render("session/forgot-password", {
+      success: "Verifique o seu email para resetar sua senha."
+    })
   }
 }
