c520bac945b04f3ec9509bcf218695849121b082
Cássia <cascrisbern@gmail.com>
2020-12-16 14:36:31 -0300

    using node crypt

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto")
+
 module.exports = {
   loginForm(req,res) {
     return res.render("session/login")
@@ -18,7 +20,24 @@ module.exports = {
     return res.render("session/forgot-password")
   },
 
-  forgot(req,res) {
-    
+  async forgot(req,res) {
+
+    const user = req.user
+
+    //fazer um token para esse usuário
+    const token = crypto.randomBytes(20).toString("hex")
+
+    //criar uma expiração para o token
+    let now = new Date()
+    now = now.setHours(now.getHours() + 1)
+
+    await User.update(user.id, {
+      reset_token: token,
+      reset_token_expires: now
+    })
+
+    //enviar um email com um link de recuperação de senha
+
+    //avisar o usuário que enviamos o email
   }
 }
