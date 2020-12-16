fcb2278ba22f5adbd29ce54f4448d1dba0fa7a19
Cássia <cascrisbern@gmail.com>
2020-12-16 14:10:49 -0300

    form to recover password

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -12,5 +12,13 @@ module.exports = {
   logout(req,res) {
     req.session.destroy()
     return res.redirect("/")
+  },
+
+  forgotForm(req,res) {
+    return res.render("session/forgot-password")
+  },
+
+  forgot(req,res) {
+    
   }
 }
